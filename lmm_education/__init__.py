1322ac400baedd7de8b93927156c15c3879de36b
Roberto Viviani home new <roberto.viviani@uibk.ac.at>
2025-11-03 10:19:56 +0100

    lmm_education init, coding started

---
+++ lmm_education/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+# type: ignore[reportUnusedImport]
+
+from lmm.scan.scan import markdown_scan as scan
+from lmm.scan.scan_messages import (
+    markdown_messages as scan_messages,
+    markdown_remove_messages as scan_remove_messages,
+)
+from lmm.scan.scan_rag import markdown_rag
+
+from lmm_education.config.config import create_default_config_file
+from lmm_education.ingest import markdown_upload as ingest
+from lmm_education.querydb import querydb
+from lmm_education.query import query
+from lmm_education.stores.vector_store_qdrant_utils import (
+    database_info,
+)
